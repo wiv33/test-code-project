abfe883df7236da3f6bc37b703de76318a1330ec
PSK <wiv821253@gmail.com>
2020-03-04 19:03:03 +0900

    inti more Pagination

---
+++ src/test/java/com/psawesome/testcodeproject/pagination/more/MorePagination.java
@@ -0,0 +1,15 @@
+package com.psawesome.testcodeproject.pagination.more;
+
+/**
+ * @author pilseong
+ * @version 1.0
+ * @description
+ * @see == 개정이력(Modification Information) ==
+ * <p>
+ * 수정일             수정자            수정내용
+ * ------          --------      --------------------------
+ * @since 2020-03-04
+ */
+
+public class MorePagination {
+}
---
+++ src/test/java/com/psawesome/testcodeproject/pagination/more/MoreRunner.java
@@ -0,0 +1,31 @@
+package com.psawesome.testcodeproject.pagination.more;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+/**
+ * @author pilseong
+ * @version 1.0
+ * @description
+ * @see == 개정이력(Modification Information) ==
+ * <p>
+ * 수정일             수정자            수정내용
+ * ------          --------      --------------------------
+ * @since 2020-03-04
+ */
+
+public class MoreRunner {
+
+    int start_index = 0, total_index = 0, list_count = 0;
+
+    @Test
+    void page_start() {
+        total_index = 32514;
+        list_count = 10;
+
+        MorePagination more = new MorePagination();
+        Map<String, Integer> map = more.getPage(total_index, list_count, start_index);
+
+    }
+}
