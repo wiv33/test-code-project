254e5e21a7f391ad93d194bbb852e3f8cb59dc90
PSK <wiv821253@gmail.com>
2020-03-09 12:57:05 +0900

    test 변경

--- src/test/java/com/psawesome/testcodeproject/bytebuddyTest/ByteBuddyTest.java
+++ src/test/java/com/psawesome/testcodeproject/bytebuddyTest/ByteBuddyTest.java
@@ -1,13 +1,19 @@
 package com.psawesome.testcodeproject.bytebuddyTest;
 
+import com.psawesome.testcodeproject.bytebuddyTest.model.MyChildClass;
 import com.psawesome.testcodeproject.bytebuddyTest.model.MyParentClass;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * package: com.psawesome.testcodeproject.lombokTest.model
@@ -17,17 +23,34 @@ import java.lang.reflect.Field;
 public class ByteBuddyTest {
 
     @Test
-    void initTest() throws IOException {
+    void parentClassTest() throws IOException {
         DynamicType.Unloaded<MyParentClass> make = new ByteBuddy()
                 .subclass(MyParentClass.class)
                 .defineField("psAwesome", String.class, Visibility.PUBLIC)
-                .defineField("isBody", Long.class, Visibility.PUBLIC)
+                .defineField("isBody", String.class, Visibility.PUBLIC)
+                .defineField("id", Long.class, Visibility.PRIVATE)
                 .make();
+//                .saveIn(new File());
 
-        for (Field field : make.load(MyParentClass.class.getClassLoader())
-                .getLoaded()
-                .getFields()) {
-            System.out.println("field.getName() = " + field.getName());
+        Class<? extends MyParentClass> loaded = make.load(MyParentClass.class.getClassLoader())
+                .getLoaded();
+
+        Object[] objects = Arrays.stream(loaded.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.toList())
+                .toArray();
+        Object[] expected = Arrays.asList("psAwesome", "isBody", "id")
+                .toArray();
+
+        Assertions.assertArrayEquals(expected, objects);
+    }
+
+    @Test
+    void childClassTest() throws ClassNotFoundException {
+        Class<?> clazz = Class.forName(MyChildClass.class.getName());
+
+        for (Field declaredField : clazz.getDeclaredFields()) {
+            System.out.println("declaredField = " + declaredField.getName());
         }
     }
 }
