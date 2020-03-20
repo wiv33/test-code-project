9ae8cbd253622b54bc8f6e350e1288821fe0be40
PSK <wiv821253@gmail.com>
2020-03-20 12:31:35 +0900

    java 14 switch

---
+++ src/test/java/com/psawesome/testcodeproject/_jdk14_/_switch_/Java14SwitchTest.java
@@ -0,0 +1,37 @@
+package com.psawesome.testcodeproject._jdk14_._switch_;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pilseong
+ * @version 1.0
+ * @description
+ * @see == 개정이력(Modification Information) ==
+ * <p>
+ * 수정일             수정자            수정내용
+ * ------          --------      --------------------------
+ * @since 2020-03-20
+ */
+
+public class Java14SwitchTest {
+
+    @Test
+    void initTest() {
+        var a = "a";
+        int num = switch (a) {
+            case "a" -> 3;
+            case "b" -> 4;
+            case "c" -> 6;
+            case "d" -> 7;
+        }
+        System.out.println("num = " + num);
+        var b = "d";
+        System.out.println(switch (b) {
+            case "a" -> 3;
+            case "b" -> 4;
+            case "c" -> 6;
+            case "d" -> 7;
+        });
+
+    }
+}
