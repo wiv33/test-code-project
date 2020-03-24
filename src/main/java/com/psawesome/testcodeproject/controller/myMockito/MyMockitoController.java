fd544f85091b3195ffd115680326162ea506b6ea
PSK <wiv821253@gmail.com>
2020-03-24 23:45:49 +0900

    -

--- src/main/java/com/psawesome/testcodeproject/controller/myMockito/MyMockitoController.java
+++ src/main/java/com/psawesome/testcodeproject/controller/myMockito/MyMockitoController.java
@@ -1,6 +1,8 @@
 package com.psawesome.testcodeproject.controller.myMockito;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * package: com.psawesome.testcodeproject.controller.myMockito
@@ -10,5 +12,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyMockitoController {
 
-
+    @GetMapping("/my-mock")
+    @ResponseBody
+    public String myMockitoTest() {
+        return "bean";
+    }
 }
