e47ef5a3a34b91029e45df0f77345501a9f32104
PSK <wiv821253@gmail.com>
2020-04-06 01:14:50 +0900

    collectors

--- src/test/java/com/psawesome/testcodeproject/failedTest/labmda/CollectStreamExam.java
+++ src/test/java/com/psawesome/testcodeproject/failedTest/labmda/CollectStreamExam.java
@@ -3,10 +3,7 @@ package com.psawesome.testcodeproject.failedTest.labmda;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,6 +60,68 @@ public class CollectStreamExam {
                 .collect(Collectors.groupingBy(MyPerson::getAge));
 
         collect.forEach((age, p) -> System.out.format("age %s : %s\n", age, p));
+    }
+
+    @Test
+    @DisplayName("P로 시작하는 객체의 집합")
+    void testCollectorsToSet() {
+        Set<MyPerson> p = myPersonList.stream()
+                .filter(m -> m.getName().startsWith("P"))
+                .collect(Collectors.toSet());
+
+        assertEquals("[MyPerson{name='PIL', age=27}]", p.toString());
+    }
+
+    @Test
+    @DisplayName("원소들의 평균 집계 만들기")
+    void testAverageGroup() {
+        Double average = myPersonList.parallelStream()
+                .collect(Collectors.averagingDouble(MyPerson::getAge));
+
+        assertEquals(22.25, average);
+    }
+
+    @Test
+    void testAverage() {
+        System.out.println( (17 * 2 + 27 + 28.0) / 4.0);
+    }
+
+    @Test
+    @DisplayName("빌트 - 인 요약 통계 객체")
+    void testCollectorsSummarizingDouble() {
+        DoubleSummaryStatistics actual = myPersonList
+                .parallelStream()
+                .collect(Collectors.summarizingDouble(MyPerson::getAge));
+
+        String expected = "DoubleSummaryStatistics{count=4, sum=89.000000, min=17.000000, average=22.250000, max=28.000000}";
+        assertEquals(expected, actual.toString());
+    }
+
+    @Test
+    @DisplayName("myPerson의 이름을 문자열로 join")
+    void testCollectorsJoining() {
+        String actual = myPersonList.parallelStream()
+                .map(MyPerson::getName)
+                .collect(Collectors.joining(" and ", "In the world ", "..."));
+
+        String expected = "In the world Natal and PIL and Netty and Raina...";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCollectorsToMap() {
+        Map<Integer, String> actual = myPersonList
+                .stream()
+                .collect(Collectors.toMap(
+                        MyPerson::getAge,
+                        MyPerson::getName,
+                        (n1, n2) -> n1 + "," + n2
+                ));
+//        마지막 BinaryOperator 가 없을 경우 중복 키 처리를 할 수 없다.
+//        java.lang.IllegalStateException: Duplicate key 17 (attempted merging values Natal and Netty)
+
+        String expected = "{17=Natal,Netty, 27=PIL, 28=Raina}";
+        assertEquals(expected, actual.toString());
     }
 }
 
