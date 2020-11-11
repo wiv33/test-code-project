7708451ed12eb2d62c6f79c8d9de0e3360263f4a
PSK <40161102+wiv33@users.noreply.github.com>
2020-11-11 15:35:02 +0900

    reset

--- src/test/java/com/psawesome/testcodeproject/functioninenum/CallByEnumTest.java
+++ src/test/java/com/psawesome/testcodeproject/functioninenum/CallByEnumTest.java
@@ -2,26 +2,61 @@ package com.psawesome.testcodeproject.functioninenum;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 class CallByEnumTest {
 
-  @Test
-  void testCallFunctionInEnum() {
+    static Map<String, String> stateMap;
+    static {
+        stateMap = new HashMap<>();
+        stateMap.put("1", "READY");
+        stateMap.put("2", "END");
+        stateMap.put("3", "START");
+    }
 
-  }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String state = br.readLine();
+        System.out.println(BaseballState.valueOf(stateMap.get(state)));
+    }
+
+    @Test
+    void testCallFunctionInEnum() throws InterruptedException {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            final String state = br.readLine();
+            System.out.println(BaseballState.valueOf(state));
+
+            return null;
+        });
+        System.out.println(Thread.currentThread().getName());
+
+        es.awaitTermination(1, TimeUnit.MINUTES);
+
+    }
 
 }
 
-
-
 enum BaseballState {
-  READY(),
-  START(),
-  END(),
+    READY(),
+    START(),
+    END();
 
+    BaseballState(Function<>) {
+    }
 }
 
 enum UserAct {
-  START(),
-  SUBMIT(),
-  QUIT()
+    START(),
+    SUBMIT(),
+    QUIT()
 }
