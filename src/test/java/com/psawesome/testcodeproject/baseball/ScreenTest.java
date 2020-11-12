95e3b08a8bd5d9e2da25bea66fec2682c6570f84
PSK <40161102+wiv33@users.noreply.github.com>
2020-11-13 01:49:39 +0900

    complete test

--- src/test/java/com/psawesome/testcodeproject/baseball/ScreenTest.java
+++ src/test/java/com/psawesome/testcodeproject/baseball/ScreenTest.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashSet;
@@ -44,37 +43,42 @@ class ScreenTest {
 
 
     @ParameterizedTest
-    @CsvSource(value = {""})
-    void testBall() {
-// TODO 들어있니?
-
+    @MethodSource("getDisplayText")
+    void testStrike(String answer, String inputText, String expectedStrike) {
+        final String strike = BaseballApi.makeStrike(answer, inputText, 0, 0);
+        Assertions.assertEquals(expectedStrike, strike);
     }
 
-    @Test
-    void testStrike() {
-//        TODO 일치하니?
+    @ParameterizedTest
+    @MethodSource("getDisplayText")
+    void testBall(String answer, String inputText, String expectedStrike, String expectedBall) {
+        assertEquals(expectedBall, BaseballApi.makeBall(answer, inputText, 0, 0));
     }
 
     @ParameterizedTest
     @DisplayName("api를 통해 문자(결과)를 받아 출력한다.")
     @MethodSource("getDisplayText")
-    void testDisplayInScreen(String answer, String userInput, String expected) {
+    void testDisplayInScreen(String answer, String inputText,
+                             String expectedStrike,
+                             String expectedBall,
+                             String expected) {
         final BaseballResponse actual = BaseballApi.call(BaseballRequest.builder()
                 .api(BaseballApi.HOW_MUCH_MATCH)
                 .info(answer)
-                .body(userInput)
+                .body(inputText)
                 .build());
-//        TODO display로 이동
-//        System.out.println(userInput);
-//        System.out.println(actual.getBody());
+        System.out.println(inputText);
+        System.out.println(actual.getBody());
         Assertions.assertEquals(expected, actual.getBody());
     }
 
     static Stream<Arguments> getDisplayText() {
         return Stream.of(
-                Arguments.of("732", "132", "1 스트라이크 1볼"),
-                Arguments.of("732", "691", ""),
-                Arguments.of("732", "437", "2볼")
+                Arguments.of("732", "132", "2 스트라이크", "", "2 스트라이크"),
+                Arguments.of("732", "691", "", "", ""),
+                Arguments.of("732", "437", "1 스트라이크", "1볼", "1 스트라이크 1볼"),
+                Arguments.of("732", "731", "2 스트라이크", "", "2 스트라이크"),
+                Arguments.of("732", "732", "", "", "3개의 숫자를 모두 맞히셨습니다. 게임 종료")
         );
     }
 
@@ -104,26 +108,44 @@ class BaseballResponse {
 @Builder
 class BaseballRequest {
     private final BaseballApi api;
-    private String info;
-    private String body;
+    private final String info;
+    private final String body;
 }
 
 enum BaseballApi {
     ANSWER(req -> {
         final int RESULT_LENGTH = 3;
         Set<String> result = new HashSet<>();
+
         do {
             final int x = ThreadLocalRandom.current().nextInt(1, 9);
             result.add(String.valueOf(x));
         } while (result.size() < RESULT_LENGTH);
+
         return new BaseballResponse("answer", result.toString().replaceAll("[^\\d]", ""));
     }),
     HOW_MUCH_MATCH(req -> {
-//        TODO find match index
-//        TODO find contains count
-//        TODO make Text???
-        return new BaseballResponse("", "1 스트라이크 1볼");
+        final String answer = req.getInfo();
+        final String inputNumbers = req.getBody();
+        if (makeOut(answer, inputNumbers)) {
+            return new BaseballResponse("end", "3개의 숫자를 모두 맞히셨습니다. 게임 종료");
+        }
+        final String strike = makeStrike(answer, inputNumbers, 0, 0);
+        final String ball = makeBall(answer, inputNumbers, 0, 0);
+        final String result = makeResult(strike, ball);
+        return new BaseballResponse("ing", result);
     });
+
+    private static boolean makeOut(String answer, String inputNumbers) {
+        return answer.equals(inputNumbers);
+    }
+
+    private static String makeResult(String strike, String ball) {
+        if (strike.isEmpty() || ball.isEmpty()) {
+            return String.format("%s%s", strike, ball);
+        }
+        return String.format("%s %s", strike, ball);
+    }
 
     Function<BaseballRequest, BaseballResponse> userAct;
 
@@ -136,8 +158,43 @@ enum BaseballApi {
                 .userAct.apply(request);
     }
 
-    private static String findMatch(String answer, String inputNumbers) {
-        return "";
+    public static String makeStrike(String answer, String inputNumbers, int idx, int strike) {
+        strike = increaseIfEqualStrike(answer, inputNumbers, idx, strike);
+        if (idx > answer.length() - 2) {
+            return concatText(strike, " 스트라이크");
+        }
+        return makeStrike(answer, inputNumbers, idx + 1, strike);
+    }
+
+    private static int increaseIfEqualStrike(String answer, String inputNumbers, int idx, int strike) {
+        if (answer.charAt(idx) == inputNumbers.charAt(idx)) {
+            return strike + 1;
+        }
+        return strike;
+    }
+
+    public static String makeBall(String answer, String inputNumbers, int idx, int ball) {
+        ball = increaseIfEqualBall(answer, inputNumbers, idx, ball);
+        if (idx > answer.length() - 2) {
+            return concatText(ball, "볼");
+        }
+        return makeBall(answer, inputNumbers, idx + 1, ball);
+    }
+
+    private static int increaseIfEqualBall(String answer, String inputNumbers, int idx, int ball) {
+        if (answer.charAt(idx) != inputNumbers.charAt(idx) &&
+                answer.contains(String.valueOf(inputNumbers.charAt(idx)))) {
+            return ball + 1;
+        }
+        return ball;
+    }
+
+    public static String concatText(int count, String text) {
+//        return count == 0 ? "" : String.format("%d%s", count, text);
+        if (count == 0) {
+            return "";
+        }
+        return String.format("%d%s", count, text);
     }
 
 }
